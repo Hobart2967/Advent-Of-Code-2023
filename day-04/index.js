8fab48186cbb55d03840038dd2fa7515c0613b0c
Hobart2967 <hobart2967@outlook.com>
2023-12-09 01:15:19 +0100

    feat(day-04): :sparkles: Add day 04

---
+++ day-04/index.js
@@ -0,0 +1,64 @@
+import { getInput } from '../util/get-input.js';
+
+async function main() {
+  const input = getInput();
+  const prep = input
+    .split('\n')
+    .map(line => mapCard(/Card\s+(\d+):\s+([^|]+)\|(.+)/g.exec(line)))
+    .map(card => ({
+      ...card,
+      owningWinCount: card.owning.filter(number => card.winning.includes(number)).length
+    }))
+    .map(card => ({
+      ...card,
+      score: card.owningWinCount === 0
+        ? 0
+        : Math.pow(2, card.owningWinCount - 1)
+    }));
+
+  console.log(JSON.stringify(prep));
+  const result = prep
+    .map(x => x.score)
+    .reduce((a, b) => a + b, 0);
+
+  console.log(`------------------------------------------`);
+  console.log(`The result is: ${JSON.stringify(result, null, 2)}`);
+}
+
+/**
+ *
+ * @param {string} info
+ * @returns {{id: number; Array<Array<{count, color}>>}}
+ */
+function mapCard(info) {
+  const [_, id, winning, owning] = info;
+
+  return {
+    id: parseInt(id),
+    winning: getAll(/\d+/g, winning)
+      .map(x => +x),
+    owning: getAll(/\d+/g, owning)
+      .map(x => +x),
+  };
+}
+
+function getAll(regex, str) {
+  const results = [];
+  let m;
+
+  while ((m = regex.exec(str)) !== null) {
+    // This is necessary to avoid infinite loops with zero-width matches
+    if (m.index === regex.lastIndex) {
+        regex.lastIndex++;
+    }
+
+    // The result can be accessed through the `m`-variable.
+    m.forEach((match, groupIndex) => {
+      results.push(match);
+    });
+  }
+
+  return results;
+}
+
+main();
